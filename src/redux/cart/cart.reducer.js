73053a3696aea1d1874485bd86c5628420d8eb2b
Vasquez <Cesar.Vasquez@xerox.com>
2020-02-04 14:56:23 -0600

    Redux Cart fixed

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import CartActionTypes from './cart.types'
+import CartActionsTypes from './cart.types';
 
 const INITIAL_STATE = {
     hidden: true
@@ -6,13 +6,13 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
+        case CartActionsTypes.TOOGLE_CART_HIDDEN:
             return{
                 ...state,
                 hidden: !state.hidden
             }
         default:
-            return state;
+            return state
     }
 }
 
