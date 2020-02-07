ace0cd0461c5a2997d26c6f9ccb8427c70837241
Vasquez <Cesar.Vasquez@xerox.com>
2020-02-07 13:59:06 -0600

    Redux checkout page

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sing-out/sign-in-out.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector'
+
 
 class App extends React.Component {
 
@@ -51,6 +53,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
+          
           <Route exact path='/signin' 
             render={() => 
                     this.props.currentUser ? (
